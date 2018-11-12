fe718dbb27f7c0883c50e668b9dc4781f6e21738
Eduardo Lemos Rocha <dudulr10@gmail.com>
2018-11-12 18:13:46 -0200

    Tabelas Relacionais Construídas

--- Model.py
+++ Model.py
@@ -25,7 +25,7 @@ def CreateTables(conn):
         InfluenciaId INTEGER REFERENCES InfluenciaExterna (Id)
         );""")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS PalavraChave (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS PalavrasChave (
         Nome VARCHAR(30) PRIMARY KEY,
         Idioma VARCHAR(15) NOT NULL
         );""")
@@ -42,16 +42,17 @@ def CreateTables(conn):
         NoticiaId INTEGER REFERENCES Noticia (Id)
         );""")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS Pessoa (
-        Id INTEGER PRIMARY KEY,
-        Nome VARCHAR(50) NOT NULL,
-        Idade INTEGER
-        );""")
-
     cursor.execute("""CREATE TABLE IF NOT EXISTS Ocupacao (
         Id INTEGER PRIMARY KEY,
         Emprego VARCHAR(40) NOT NULL,
         Descricao VARCHAR(100)
+        );""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS Pessoa (
+        Id INTEGER PRIMARY KEY,
+        Nome VARCHAR(50) NOT NULL,
+        Idade INTEGER,
+        OcupacaoId INTEGER REFERENCES Ocupacao (Id)
         );""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS CategoriaMidia (
@@ -63,7 +64,45 @@ def CreateTables(conn):
     cursor.execute("""CREATE TABLE IF NOT EXISTS Midia (
         Id INTEGER PRIMARY KEY,
         Nome VARCHAR(50) NOT NULL,
-        Descricao VARCHAR(250)
+        Descricao VARCHAR(250),
+        CategoriaId INTEGER REFERENCES CategoriaMidia (Id)
+        );""")
+
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS Midia_Noticia (
+        NoticiaId INTEGER REFERENCES Noticia (Id),
+        MidiaId INTEGER REFERENCES Midia (Id),
+        PRIMARY KEY (NoticiaId, MidiaId)
+        );""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS PalavrasChave_Noticia (
+        NoticiaId INTEGER REFERENCES Noticia (Id),
+        PalavrasChaveId VARCHAR(30) REFERENCES PalavrasChave (Nome),
+        PRIMARY KEY (NoticiaId, PalavrasChaveId)
+        );""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS Local_Noticia (
+        NoticiaId INTEGER REFERENCES Noticia (Id),
+        LocalId CHAR(2) REFERENCES Local (Sigla),
+        PRIMARY KEY (NoticiaId, LocalId)
+        );""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS CategoriaNoticia_Noticia (
+        NoticiaId INTEGER REFERENCES Noticia (Id),
+        CategoriaId INTEGER REFERENCES CategoriaNoticia (Id),
+        PRIMARY KEY (NoticiaId, CategoriaId)
+        );""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS Autor_Noticia (
+        NoticiaId INTEGER REFERENCES Noticia (Id),
+        AutorId INTEGER REFERENCES Pessoa (Id),
+        PRIMARY KEY (NoticiaId, AutorId)
+        );""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS Vitima_Noticia (
+        NoticiaId INTEGER REFERENCES Noticia (Id),
+        VitimaId INTEGER REFERENCES Pessoa (Id),
+        PRIMARY KEY (NoticiaId, VitimaId)
         );""")
 
     cursor.close()
